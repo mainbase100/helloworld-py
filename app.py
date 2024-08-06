5d13418ba9244a9e5f0dbb18767307974938555b
mainbase100 <youshud86@gmail.com>
2024-08-06 13:40:56 +0900

    追加

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
+import time
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+@app.route('/name')
+def get_name():
+	return 'AWS MASTERCHEF'
+
+@app.route('/version')
+def get_name():
+	return 'v1.0.0'
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0', port=8000)
